8c44ced61067fb2d9698f903bf1289eae28f7354
Maulana Yusuf Habibi <64534810+maulanayusufhabibi@users.noreply.github.com>
2022-04-13 21:08:55 +0700

    Update input.h

--- StudiKasus4/library/input.h
+++ StudiKasus4/library/input.h
@@ -5,24 +5,23 @@ class Input{
         void cetak(){
           cout << "APLIKASI MENGHITUNG UANG ANDI \n";
           cout << "================================\n";
-          cout << "Uang Saku 1 bulan     : "; cin>>saku;
-    			cout << "Jumlah bulan          : "; cin>>jml_bulan;
+          cout << "Uang Saku Per bulan     : "; cin>>saku;
+    			cout << "Jumlah bulan            : "; cin>>jml_bulan;
           cout << "================================\n";
           for(int i = 1; i<=jml_bulan; i++){
             cout << "Bulan ke " << i << endl;
-    			  cout << "Biaya Kuliah          : "; cin>>duit_kuliah;
-            cout << "Biaya Jajan           : "; cin>>duit_jajan;
-            cout << "Jumlah yang ditabung  : "; cin>>jml_nabung;
+            cout << "Pengeluaran          : "; cin>>pengeluaran[i];
             cout << "=================================\n";
           }
 		}	
 
 		void toFile(){
-			tulis_data.open("../pra_data/input.txt");
+			tulis_data.open("api_data.txt");
+      tulis_data << saku << endl;
       tulis_data << jml_bulan << endl;
-      tulis_data << duit_kuliah << endl;
-      tulis_data << duit_jajan << endl;
-			tulis_data << jml_nabung;
+      for(int i = 1; i<=jml_bulan; i++){
+    		tulis_data << pengeluaran[i]<<endl;  
+      }
 			tulis_data.close();
 		}
 
@@ -30,7 +29,5 @@ class Input{
 		ofstream tulis_data;
     int saku;
 		int jml_bulan;
-    int duit_kuliah;
-    int duit_jajan;
-    int jml_nabung;
+    int pengeluaran[20];
 };
