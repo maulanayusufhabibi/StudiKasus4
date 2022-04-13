d434a997a2d551758db2d42a7609f872a7015ee0
Maulana Yusuf Habibi <64534810+maulanayusufhabibi@users.noreply.github.com>
2022-04-13 21:07:22 +0700

    Update proses.h

--- StudiKasus4/library/proses.h
+++ StudiKasus4/library/proses.h
@@ -7,82 +7,43 @@ class Proses{
 		}
 
 		void getData(){
-		ambil_data.open("../dummy/out_input.txt");
-			bool pengeluaran = true;
-      ambil_data 
-			for(int i=0; i<=bulan; i++){
-        ambil_da
-				if (bulan =1){
-					// cout<<"Masukkan Pengeluaran PerBulan : ";
-     //      ambil_data>>pengeluaran;
-     //      cout<<"Masukkan Total pengeluaran : ";
-     //      ambil_data>>keluar;
-     //      cout<<"Masukkan Pengeluaran Kuliah : ";
-     //      ambil_data>>keluar_Kuliah;
-     //      cout<<"Masukkan Pengeluaran Jajan : ";
-     //      ambil_data>>keluar_jajan;
-     //      cout<<"Masukkan Tabungan perbulan : ";
-     //      ambil_data>>tabungan;
-          ambil_data>>
-				}
-        else if (bulan = 2){
-					cout<<"Masukkan Pengeluaran PerBulan : ";
-          ambil_data>>pengeluaran;
-          cout<<"Masukkan Total pengeluaran : ";
-          ambil_data>>keluar;
-          cout<<"Masukkan Pengeluaran Kuliah : ";
-          ambil_data>>keluar_Kuliah;
-          cout<<"Masukkan Pengeluaran Jajan : ";
-          ambil_data>>keluar_jajan;
-          cout<<"Masukkan Tabungan perbulan : ";
-          ambil_data>>tabungan;
-				}
-        else if (bulan = 3){
-					cout<<"Masukkan Pengeluaran PerBulan : ";
-          ambil_data>>pengeluaran;
-          cout<<"Masukkan Total pengeluaran : ";
-          ambil_data>>keluar;
-          cout<<"Masukkan Pengeluaran Kuliah : ";
-          ambil_data>>keluar_Kuliah;
-          cout<<"Masukkan Pengeluaran Jajan : ";
-          ambil_data>>keluar_jajan;
-          cout<<"Masukkan Tabungan perbulan : ";
-          ambil_data>>tabungan;
-				}
-				
-			}
-			//ambil_data.close();
-		
+		  ambil_data.open("api_data.txt");
+          ambil_data >> saku;
+          ambil_data >> jml_bulan;
+          for(int i = 1; i<=jml_bulan; i++){
+    			  ambil_data >> pengeluaran[i];  
+          }  
+          ambil_data.close();
+      }	
 
 		void toFile(){
-		 total_pengeluaran = saku -(pengeluaran + keluar + keluar_kuliah + keluar_jajan + tabungan)
-      total_tabungan += tabung; 
-          
-	    (t2 = (total + ongkir - diskon));
+      saku *= jml_bulan;
+          for(int i = 1; i<=jml_bulan; i++){
+        		total_pengeluaran += pengeluaran[i];
+            total_tabungan = saku - total_pengeluaran;
+          } 
+    
 
+			tulis_data.open("api_data.txt");
+			tulis_data << saku << endl;
+      tulis_data << jml_bulan << endl;
+      for(int i = 1; i<=jml_bulan; i++){
+        tulis_data << pengeluaran[i]<<endl;
+      }
+			
+			tulis_data << total_pengeluaran << endl;
+			tulis_data << total_tabungan;
 
-
-			// tulis_data.open("../dummy/proses_input.txt");
-			// tulis_data << total << endl;
-   //    tulis_data << jarak << endl;      
-			// tulis_data << diskon << endl;
-			// tulis_data << t2 << endl;
-			// tulis_data << bnyk_aymGpr << endl;
-			// tulis_data << bnyk_aymGr <<endl;
-   //    tulis_data << bnyk_udGr <<endl;
-   //    tulis_data << bnyk_cmGr <<endl;
-   //    tulis_data << bnyk_aymBk;
-			// tulis_data.close();
+		  tulis_data.close();
 		}
 
 	private :
 		ifstream ambil_data;
 		ofstream tulis_data;
-
-		int saku = 500000;
-    int pengeluaran;
-    int keluar, keluar_kuliah;
-    int tabungan;
-    int totalPengeluaran;
-    int total;
+    int i, saku;
+    int jml_bulan;
+    int pengeluaran[20];
+    int total_pengeluaran = 0;
+    int total_tabungan = 0;
+    
 };
